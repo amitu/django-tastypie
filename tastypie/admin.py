4a1336db92a71bfb324ffd284ca708f46f2d508a
Alejandro Varas <alej0varas@gmail.com>
2013-02-27 12:49:36 -0800

    Updated admin to load work with custom user model

--- tastypie/admin.py
+++ tastypie/admin.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 
 
-if 'django.contrib.auth' in settings.INSTALLED_APPS:
+if hasattr(settings, 'AUTH_APP') and settings.AUTH_APP in settings.INSTALLED_APPS and hasattr(settings, 'AUTH_USER_MODEL'):
     from tastypie.models import ApiKey
 
     class ApiKeyInline(admin.StackedInline):
